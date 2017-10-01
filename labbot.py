a49caeae580f6890318ee57e1e02236da2ced0f8
XianClasen <me@christianclasen.com>
2017-10-01 16:18:00 -0400

    Added docstring and __main__.

--- labbot.py
+++ labbot.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+'''
+A simple Python Twitter bot for managing power in my CCIE lab.
+Will accept control messages from a specific Twitter account in the form of mentions.
+
+Author: Christian Clasen
+'''
+
 import datetime
 import logging
-import mentionlib
-import Robot
 import subprocess
 import time
+import mentionlib
+import Robot
 
 robot = Robot.Robot()
 
@@ -13,7 +21,6 @@ def shutdown():
                            shell=False,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
-    # ssh.kill()
 
 
 def ping(host):
@@ -52,104 +59,104 @@ lastmentiondict = mentionlib.parsementionjson(lastmentionjson)
 initmentionid = mentionlib.initmentionid(lastmentiondict)
 logging.debug("Initial mention ID: " + str(initmentionid))
 
-# Main program
-while True:
-    try:
+if __name__ == '__main__':
+    while True:
+        try:
 
-        # Variables for timestamp
-        epochtime = time.time()
-        humantime = datetime.datetime.fromtimestamp(epochtime).strftime('%Y-%m-%d %H:%M:%S')
+            # Variables for timestamp
+            epochtime = time.time()
+            humantime = datetime.datetime.fromtimestamp(epochtime).strftime('%Y-%m-%d %H:%M:%S')
 
-        # Wait 60 seconds between polls
-        time.sleep(60)
+            # Wait 60 seconds between polls to avoid Twitter's API rate limit
+            time.sleep(60)
 
-        # Setup the last mention variables
-        lastmentionjson = mentionlib.getlastmentionjson()
-        lastmentiondict = mentionlib.parsementionjson(lastmentionjson)
-        lastmentionscreenname = mentionlib.getmentionscreenname(lastmentiondict)
-        lastmentionid = mentionlib.getlastmentionid(lastmentiondict)
-        lastmentiontext = mentionlib.getlastmentiontext(lastmentiondict)
-        mentionerstatus = mentionlib.verifymentioner(lastmentionscreenname)
-        idstatus = mentionlib.verifymentionid(lastmentionid, initmentionid)
+            # Setup the last mention variables
+            lastmentionjson = mentionlib.getlastmentionjson()
+            lastmentiondict = mentionlib.parsementionjson(lastmentionjson)
+            lastmentionscreenname = mentionlib.getmentionscreenname(lastmentiondict)
+            lastmentionid = mentionlib.getlastmentionid(lastmentiondict)
+            lastmentiontext = mentionlib.getlastmentiontext(lastmentiondict)
+            mentionerstatus = mentionlib.verifymentioner(lastmentionscreenname)
+            idstatus = mentionlib.verifymentionid(lastmentionid, initmentionid)
 
-        logging.debug('Last mention ID: ' + str(lastmentionid) + ' Last mention text: ' + str(lastmentiontext))
+            logging.debug('Last mention ID: ' + str(lastmentionid) + ' Last mention text: ' + str(lastmentiontext))
 
-        # If the mentioner is allowed, and the ID is new
-        if (mentionerstatus == 'Verified') and (idstatus == 'Verified'):
+            # If the mentioner is allowed, and the ID is new
+            if (mentionerstatus == 'Verified') and (idstatus == 'Verified'):
 
-            # Log who issued us what command
-            logging.info(humantime + ': ' + lastmentionscreenname + ' issued us the following command: ' + lastmentiontext)
+                # Log who issued us what command
+                logging.info(humantime + ': ' + lastmentionscreenname + ' issued us the following command: ' + lastmentiontext)
 
-            # Reinitialize the ID
-            initmentionid = lastmentionid
+                # Reinitialize the ID
+                initmentionid = lastmentionid
 
-            # Answer a basic health check query
-            if '@LabPiBot Hello' in lastmentiontext:
+                # Answer a basic health check query
+                if '@LabPiBot Hello' in lastmentiontext:
 
-                mantionlib.createtweet("@XianClasen Is it me you're looking for? " + humantime)
+                    mantionlib.createtweet("@XianClasen Is it me you're looking for? " + humantime)
 
-            # Switch on the lab
-            elif '@LabPiBot Poweron' in lastmentiontext:
+                # Switch on the lab
+                elif '@LabPiBot Poweron' in lastmentiontext:
 
-                switch_on()
-                mentionlib.createtweet('@XianClasen I switched the lab on.  Give me some time to verify that it worked. ' + humantime)
-                logging.info(humantime + ': Tweet created: I switched the lab on.  Give me some time to verify that it worked.')
+                    switch_on()
+                    mentionlib.createtweet('@XianClasen I switched the lab on.  Give me some time to verify that it worked. ' + humantime)
+                    logging.info(humantime + ': Tweet created: I switched the lab on.  Give me some time to verify that it worked.')
 
-                i = 0
-                hostisup = False
-                while not hostisup:
+                    i = 0
+                    hostisup = False
+                    while not hostisup:
 
-                    time.sleep(30)
-                    i = i + 1
+                        time.sleep(30)
+                        i = i + 1
 
-                    if '1 receive' in ping('esxi'):
-                        hostisup = True
-                        logging.info(humantime + 'Got a ping reply from esxi.')
+                        if '1 receive' in ping('esxi'):
+                            hostisup = True
+                            logging.info(humantime + 'Got a ping reply from esxi.')
 
-                    elif i > 20:
-                        mentionlib.createtweet("@XianClasen I have failed you.  The lab isn't up. " + humantime)
-                        logging.info(humantime + ": Tweet created: I have failed you.  The lab isn't up.")
-                        break
+                        elif i > 20:
+                            mentionlib.createtweet("@XianClasen I have failed you.  The lab isn't up. " + humantime)
+                            logging.info(humantime + ": Tweet created: I have failed you.  The lab isn't up.")
+                            break
 
-                if hostisup:
-                    mentionlib.createtweet('@XianClasen I verified that the lab is up. ' + humantime)
-                    logging.info(humantime + ': Tweet created: I verified that the lab is up. ')
+                    if hostisup:
+                        mentionlib.createtweet('@XianClasen I verified that the lab is up. ' + humantime)
+                        logging.info(humantime + ': Tweet created: I verified that the lab is up. ')
 
-            # Shutdown and switchoff the lab
-            elif '@LabPiBot Poweroff' in lastmentiontext:
+                # Shutdown and switchoff the lab
+                elif '@LabPiBot Poweroff' in lastmentiontext:
 
-                shutdown()
-                mentionlib.createtweet("@XianClasen I told ESXI to shutdown.  I'll let you know when I've switched off the power. " + humantime)
-                logging.info(humantime + ": Tweet created: I told ESXI to shutdown.  I'll let you know when I've switched off the power.")
+                    shutdown()
+                    mentionlib.createtweet("@XianClasen I told ESXI to shutdown.  I'll let you know when I've switched off the power. " + humantime)
+                    logging.info(humantime + ": Tweet created: I told ESXI to shutdown.  I'll let you know when I've switched off the power.")
 
-                # Ping ESXI until it stops responding, or we reach 10 minutes
-                hostisup = True
-                i = 0
+                    # Ping ESXI until it stops responding, or we reach 10 minutes
+                    hostisup = True
+                    i = 0
 
-                while hostisup:
+                    while hostisup:
 
-                    i = i + 1
-                    time.sleep(30)
+                        i = i + 1
+                        time.sleep(30)
 
-                    if ('0 receive' in ping('esxi')) or (i > 20):
-                        hostisup = False
+                        if ('0 receive' in ping('esxi')) or (i > 20):
+                            hostisup = False
 
-                # Turn off the power
-                logging.info(humantime + ': Switching off the power now.')
-                switch_off()
-                mentionlib.createtweet("@XianClasen I've switched off the power. " + humantime)
+                    # Turn off the power
+                    logging.info(humantime + ': Switching off the power now.')
+                    switch_off()
+                    mentionlib.createtweet("@XianClasen I've switched off the power. " + humantime)
 
-        # In case the mentioner isn't trusted
-        elif mentionerstatus == 'Unverified':
+            # In case the mentioner isn't trusted
+            elif mentionerstatus == 'Unverified':
 
-            logging.warning(lastmentionscreenname + ' tried to issue us the following command: ' + lastmentiontext)
-            print(lastmentionscreenname + ' tried to issue us the following command: ' + lastmentiontext)
-            mentionlib.createtweet("@" + lastmentionscreenname + " You didn't say the magic word! " + humantime)
-            logging.info(humantime + ": Tweet created: " + lastmentionscreenname + " You didn't say the magic word!")
+                logging.warning(lastmentionscreenname + ' tried to issue us the following command: ' + lastmentiontext)
+                print(lastmentionscreenname + ' tried to issue us the following command: ' + lastmentiontext)
+                mentionlib.createtweet("@" + lastmentionscreenname + " You didn't say the magic word! " + humantime)
+                logging.info(humantime + ": Tweet created: " + lastmentionscreenname + " You didn't say the magic word!")
 
-            # Reinitialize the last mention ID
-            initmentionid = lastmentionid
+                # Reinitialize the last mention ID
+                initmentionid = lastmentionid
 
-    except:
-        logging.exception('Got exception on main program.')
-        raise
+        except:
+            logging.exception('Got exception on main program.')
+            raise
